57dc42fd17eab0f38c2aeb55fa55d1e49ede53c3
milenbachev <63249976+milenbachev@users.noreply.github.com>
2021-12-11 19:03:45 +0200

    add guestButton and userButton in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,38 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+
+import { AuthContext } from '../../contexts/AuthContext.js';
 
 import './Header.css';
 
 function Header() {
+    const { user } = useContext(AuthContext);
+
+    const guestButton = (
+        <>
+            <li className="header-nav-ul-li">
+                <Link className="header-nav-ul-li-item" to="/login">Login</Link>
+            </li>
+            <li className="header-nav-ul-li">
+                <Link className="header-nav-ul-li-item" to="/register">Register</Link>
+            </li>
+        </>
+    )
+    
+    const userButton = (
+        <>
+            <li className="header-nav-ul-li">
+                <Link className="header-nav-ul-li-item" to="/my-cars">My-cars</Link>
+            </li>
+            <li className="header-nav-ul-li">
+                <Link className="header-nav-ul-li-item" to="/create">Create</Link>
+            </li>
+            <li className="header-nav-ul-li">
+                <Link className="header-nav-ul-li-item" to="/logout">Logout</Link>
+            </li>
+        </>
+    )
+
     return (
         <header className='header-conteiner'>
             <nav className="header-conteiner-nav">
@@ -11,23 +41,12 @@ function Header() {
                     <div className="header-conteiner-nav-div" >
                         <ul className="header-nav-ul">
                             <li className="header-nav-ul-li">
-                                <Link className="header-nav-ul-li-item"  to="/">Home</Link>
+                                <Link className="header-nav-ul-li-item" to="/">Home</Link>
                             </li>
-                            <li className="header-nav-ul-li">
-                                <Link className="header-nav-ul-li-item" to="/login">Login</Link>
-                            </li>
-                            <li className="header-nav-ul-li">
-                                <Link className="header-nav-ul-li-item" to="/register">Register</Link>
-                            </li>
-                            <li className="header-nav-ul-li">
-                                <Link className="header-nav-ul-li-item" to="/my-cars">My-cars</Link>
-                            </li>
-                            <li className="header-nav-ul-li">
-                                <Link className="header-nav-ul-li-item" to="/create">Create</Link>
-                            </li>
-                            <li className="header-nav-ul-li">
-                                <Link className="header-nav-ul-li-item" to="/logout">Logout</Link>
-                            </li>
+                            {user.email
+                                ? guestButton
+                                : userButton
+                            }
                         </ul>
                     </div>
                 </div>
