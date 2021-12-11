df0899cb3ceddd87c129443d8734fa8dec57c2b8
milenbachev <63249976+milenbachev@users.noreply.github.com>
2021-12-11 22:45:37 +0200

    add css for register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,14 @@
-import {Link, useNavigate} from 'react-router-dom';
-import {useContext} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 
-import {AuthContext} from '../../contexts/AuthContext.js';
-import * as authService from '../../services/authService.js'
+import { AuthContext } from '../../contexts/AuthContext.js';
+import * as authService from '../../services/authService.js';
+
+import './Register.css';
 
 function Register() {
     const navigate = useNavigate();
-    const {login} = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
 
     const onRegisterHandler = (e) => {
         e.preventDefault();
@@ -17,7 +19,7 @@ function Register() {
         let password = formData.get('password');
 
         authService.register(email, password)
-            .then(authData =>{
+            .then(authData => {
                 login(authData)
                 navigate('/')
             })
@@ -26,37 +28,37 @@ function Register() {
             })
     }
     return (
-        <div className="container">
-        <div className="card">
-            <div className="card-header">
-                <h3>Register</h3>
-            </div>
-            <div className="card-body">
-                <form id="register-form" method='POST' onSubmit={onRegisterHandler}>
-                    <div className='field'>
-                        <label className='field-label' htmlFor='email'>Email</label>
-                        <input type="text" className="form-control" name='email' />
+        <div className="container-register">
+            <div className="card-register">
+                <div className="card-header">
+                    <h3>Register</h3>
+                </div>
+                <div className="card-body">
+                    <form id="register-form" method='POST' onSubmit={onRegisterHandler}>
+                        <div className='field'>
+                            <label className='field-label' htmlFor='email'>Email</label>
+                            <input type="text" className="form-control" name='email' />
+                        </div>
+                        <div className='field'>
+                            <label className='field-label' htmlFor='password'>Password</label>
+                            <input type="password" className="form-control" name='password' />
+                        </div>
+                        <div className='field'>
+                            <label className='field-label' htmlFor='repet-password'>Confirm</label>
+                            <input type="password" className="form-control" name='repet-password' />
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" value="Register" className="btn" />
+                        </div>
+                    </form>
+                </div>
+                <div className="card-footer">
+                    <div className="card-footer-links">
+                        You have an account?  <Link to="/login">Register</Link>
                     </div>
-                    <div className='field'>
-                        <label className='field-label' htmlFor='password'>Password</label>
-                        <input type="password" className="form-control" name='password' />
-                    </div>
-                    <div className='field'>
-                        <label className='field-label' htmlFor='repet-password'>Confirm</label>
-                        <input type="password" className="form-control" name='repet-password' />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Register" className="btn" />
-                    </div>
-                </form>
-            </div>
-            <div className="card-footer">
-                <div className="card-footer-links">
-                    You have an account?  <Link to="/login">Sign Up</Link>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
