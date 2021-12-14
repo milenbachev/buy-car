245f090ee5eaddc6c44e3b7b3c4714e892a9f96e
milenbachev <63249976+milenbachev@users.noreply.github.com>
2021-12-14 19:11:55 +0200

    import react-boostrapt

--- src/components/CarCard/CarCard.js
+++ src/components/CarCard/CarCard.js
@@ -1,4 +1,5 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {Card, Button} from 'react-bootstrap'
 
 import './CarCard.css'
 
@@ -6,16 +7,28 @@ function CarCard({
     car
 }) {
     return (
-        <div className="card">
-            <img className="card-img-top" src={car.img} alt={car.model}/>
-                <div className="card-body">
-                    <h5 className="card-title">{car.brand} {car.model}</h5>
-                    <p className="card-text">Year: {car.year}</p>
-                    <p className="card-text">Engine: {car.engine}</p>
-                    <Link to={`/details/${car._id}`} className="btn btn-primary">Details</Link>
-                </div>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={car.img} alt={car.model} />
+            <Card.Body>
+                <Card.Title>{car.brand} {car.model}</Card.Title>
+                <Card.Text>Year: {car.year}</Card.Text>
+                <Card.Text>Price: {car.price}</Card.Text>
+                <Link to={`/details/${car._id}`}>
+                    <Button variant="primary">Details</Button>
+                </Link>
+            </Card.Body>
+        </Card>
     )
 }
 
 export default CarCard
+
+//<div className="card">
+//<img className="card-img-top" src={car.img} alt={car.model}/>
+    //<div className="card-body">
+        //<h5 className="card-title">{car.brand} {car.model}</h5>
+        //<p className="card-text">Year: {car.year}</p>
+        //<p className="card-text">Engine: {car.engine}</p>
+        //<Link to={`/details/${car._id}`} className="btn btn-primary">Details</Link>
+    ///</div>
+//</div>
